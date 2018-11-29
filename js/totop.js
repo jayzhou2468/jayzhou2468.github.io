3df3aa7771d61b9acdfce61034ae2c77fdd7b637
jayzhou2468 <1119120376@qq.com>
2018-11-29 18:16:19 +0800

    Site updated: 2018-11-29 18:16:17

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 29 2018 18:12:33 GMT+0800 (GMT+08:00)
+// build time:Thu Nov 29 2018 18:16:13 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
