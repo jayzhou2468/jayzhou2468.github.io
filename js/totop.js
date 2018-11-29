8cf5cfed7c411b5ae7d5c2039cfbb219e29c303d
jayzhou2468 <1119120376@qq.com>
2018-11-29 17:25:27 +0800

    Site updated: 2018-11-29 17:25:25

--- js/totop.js
+++ js/totop.js
@@ -1,13 +1,3 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
+// build time:Thu Nov 29 2018 17:25:03 GMT+0800 (GMT+08:00)
+$(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
+//rebuild by neat 
