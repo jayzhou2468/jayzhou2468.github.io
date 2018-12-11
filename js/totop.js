76154a716fde11673db7af364c7676c982261a94
jayzhou2468 <1119120376@qq.com>
2018-12-11 15:25:50 +0800

    Site updated: 2018-12-11 15:25:48

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 15:24:08 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 11 2018 15:25:44 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
