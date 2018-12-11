6ff4bf3bfa163375c154bf2b19c062cf9f27f280
jayzhou2468 <1119120376@qq.com>
2018-12-11 12:55:32 +0800

    Site updated: 2018-12-11 12:55:29

--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 12:54:17 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 11 2018 12:55:25 GMT+0800 (GMT+08:00)
 var toc=document.getElementById("toc");if(toc!=null){window.addEventListener("scroll",scrollcatelogHandler);var tocPosition=toc.offsetTop;var height_header=$("#signature").height();function scrollcatelogHandler(o){var t=o||window.event,e=t.target||t.srcElement;var c=document.documentElement.scrollTop||document.body.scrollTop;if(c>tocPosition-60){toc.classList.add("toc-fixed")}else{toc.classList.remove("toc-fixed")}}}
 //rebuild by neat 
