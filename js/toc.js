fe2481eef31d6d883738d04059914b802e59fe98
jayzhou2468 <1119120376@qq.com>
2018-12-18 11:54:19 +0800

    Site updated: 2018-12-18 11:54:15

--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 15:25:44 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 18 2018 11:54:11 GMT+0800 (GMT+08:00)
 var toc=document.getElementById("toc");if(toc!=null){window.addEventListener("scroll",scrollcatelogHandler);var tocPosition=toc.offsetTop;var height_header=$("#signature").height();function scrollcatelogHandler(o){var t=o||window.event,e=t.target||t.srcElement;var c=document.documentElement.scrollTop||document.body.scrollTop;if(c>tocPosition-60){toc.classList.add("toc-fixed")}else{toc.classList.remove("toc-fixed")}}}
 //rebuild by neat 
--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 15:25:44 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 18 2018 11:54:11 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
