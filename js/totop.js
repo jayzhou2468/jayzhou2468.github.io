ac5436430c4000ce79e7ae2cf4c4f8174d5a79d2
jayzhou2468 <1119120376@qq.com>
2019-02-20 16:26:31 +0800

    Site updated: 2019-02-20 16:26:26

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 19 2019 16:29:40 GMT+0800 (GMT+08:00)
+// build time:Wed Feb 20 2019 16:26:19 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>$(window).height()*.5?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
